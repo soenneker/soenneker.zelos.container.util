cc8a0c26b93c96d45670682fb9e8fa92a5013caa
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:54 -0500

    xml commenting

--- src/Soenneker.Zelos.Container.Util/Abstract/IZelosContainerUtil.cs
+++ src/Soenneker.Zelos.Container.Util/Abstract/IZelosContainerUtil.cs
@@ -10,6 +10,13 @@ namespace Soenneker.Zelos.Container.Util.Abstract;
 /// </summary>
 public interface IZelosContainerUtil : IAsyncDisposable, IDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="filePath">The file path.</param>
+    /// <param name="containerName">The container name.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<IZelosContainer> Get(string filePath, string containerName, CancellationToken cancellationToken = default);
 
 }
